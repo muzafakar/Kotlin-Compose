b2c6bf8c314b2a0fd31fee93657a0f1fec7e395c
muzafakar <muhammad.zulfakar@students.amikom.ac.id>
2021-09-04 22:45:55 +0800

    re-init

--- app/src/main/java/com/aslan/learncompose/MainActivity.kt
+++ app/src/main/java/com/aslan/learncompose/MainActivity.kt
@@ -3,17 +3,8 @@ package com.aslan.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.aslan.learncompose.ui.BottomNavigationBar
-import com.aslan.learncompose.ui.Navigation
-import com.aslan.learncompose.ui.TopBar
 import com.aslan.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,21 +14,9 @@ class MainActivity : ComponentActivity() {
             LearnComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun MainScreen() {
-    val navController = rememberNavController()
-
-    Scaffold(
-        topBar = { TopBar() },
-        bottomBar = { BottomNavigationBar(navController) }
-    ) {
-        Navigation(navController)
-    }
-}
